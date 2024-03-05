c82274fc6bc764712b7cca4c8c605fddf3976a2b
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-05 11:30:27 +0100

    Adjust savings format, and sort

--- methods/cw-savings.py
+++ methods/cw-savings.py
@@ -1,4 +1,5 @@
 import vrplib
+from operator import itemgetter
 
 data = vrplib.read_instance('instances/A/A-n32-k5.vrp')
 
@@ -6,10 +7,15 @@ def cw_savings(data):
     # Initialise to a single node in each route
     routes = [[i] for i in range(1,data['dimension'])]
 
-    # Calculates savings
+    # Calculate savings
     def saving(i,j):
         return data['edge_weight'][i][0]+data['edge_weight'][0][j]-data['edge_weight'][i][j]
 
     savings = []
     for i in range(2,data['dimension']):
-        savings.append([saving(i,j) for j in range(1,i)])
+        for j in range(1, i):
+            savings.append((i,j,saving(i,j)))
+    savings.sort(key=itemgetter(2), reverse=True)
+
+
+
