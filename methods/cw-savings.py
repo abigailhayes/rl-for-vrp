c096ea8a9e0a529cbfc13a79fd21ea34e08f129d
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-05 11:10:57 +0100

    Calculate savings

--- methods/cw-savings.py
+++ methods/cw-savings.py
@@ -6,3 +6,10 @@ def cw_savings(data):
     # Initialise to a single node in each route
     routes = [[i] for i in range(1,data['dimension'])]
 
+    # Calculates savings
+    def saving(i,j):
+        return data['edge_weight'][i][0]+data['edge_weight'][0][j]-data['edge_weight'][i][j]
+
+    savings = []
+    for i in range(2,data['dimension']):
+        savings.append([saving(i,j) for j in range(1,i)])
