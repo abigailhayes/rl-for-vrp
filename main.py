c6ea735fda9828f17543f86ba90ee6e6d12d14ee
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-06 09:28:01 +0100

    util for data loading

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import vrplib
+import instances.utils as instance_utils
 import methods.cw_savings as cw_savings
 
-data = vrplib.read_instance('instances/A/A-n32-k5.vrp')
-solution = vrplib.read_solution('instances/A/A-n32-k5.sol')
+data = instance_utils.import_instance('A', 'A-n32-k5')
 
-test = cw_savings.CWSavings(data)
-test.add_sol(solution)
+test = cw_savings.CWSavings(data['instance'])
+test.add_sol(data['solution'])
 test.run_all()
 print(test.cost, " Perc worse: ", '{:.1%}'.format(test.perc))
