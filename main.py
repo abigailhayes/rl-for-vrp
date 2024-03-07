dce40f918e261f32aacc72a93b34905f6df9d1e6
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-07 11:48:26 +0100

    Avoid breaking change to filepath

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import instances.utils as instances_utils
 import methods.cw_savings as cw_savings
 
-data = instances_utils.import_instance('A', 'A-n32-k5')
+data = instances_utils.import_instance('instances/A', 'A-n32-k5')
 
 test = cw_savings.CWSavings(data['instance'])
 test.add_sol(data['solution'])
