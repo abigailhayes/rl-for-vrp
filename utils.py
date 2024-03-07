8066f8635dfd02e8c98df861d43add0137becb28
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-07 12:48:30 +0100

    Results and perc results

--- utils.py
+++ utils.py
@@ -37,24 +37,30 @@ def avg_perf(task, method):
     - task; CVRP or other
     - method; the algorithm being tested"""
     directory = get_dir(task)
+    perc_results = {}
+    perc_averages = {}
     results = {}
     averages = {}
     for subdir in next(os.walk(directory))[1]:
-        results[subdir] = {}
+        perc_results[subdir] = {}
         for example in [example[:-4] for example in next(os.walk(f'{directory}/{subdir}'))[2] if example.endswith('vrp')]:
             instance = instances_utils.import_instance(f'{directory}/{subdir}', example)
             run = apply_method(method, instance)
-            results[subdir][example] = run.perc
+            results[subdir][example] = run.cost
+            perc_results[subdir][example] = run.perc
         averages[subdir] = Series([*results[subdir].values()]).mean()
-    averages['all'] = mean(NestedDictValues(results))
+        perc_averages[subdir] = Series([*perc_results[subdir].values()]).mean()
+    perc_averages['all'] = mean(NestedDictValues(perc_results))
 
-    # Save all results in a file
+    # Save all outputs in files
     os.makedirs(f'results/{task}', exist_ok=True)
     os.makedirs(f'results/{task}/{method}', exist_ok=True)
-    with open(f'results/{task}/{method}/all_instances.json', 'w') as f:
+    with open(f'results/{task}/{method}/perc_results.json', 'w') as f:
+        json.dump(perc_results, f, indent=2)
+    with open(f'results/{task}/{method}/perc_averages.json', 'w') as f:
+        json.dump(perc_averages, f, indent=2)
+    with open(f'results/{task}/{method}/results.json', 'w') as f:
         json.dump(results, f, indent=2)
-
-    # Save averages
     with open(f'results/{task}/{method}/averages.json', 'w') as f:
         json.dump(averages, f, indent=2)
 
