d13833068d6a861cc4074d715f4fd6a5840448aa
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-07 10:46:08 +0100

    Update import naming convention

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import instances.utils as instance_utils
+import instances.utils as instances_utils
 import methods.cw_savings as cw_savings
 
-data = instance_utils.import_instance('A', 'A-n32-k5')
+data = instances_utils.import_instance('A', 'A-n32-k5')
 
 test = cw_savings.CWSavings(data['instance'])
 test.add_sol(data['solution'])
