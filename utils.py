d88eb4fc23649c2e0cc93583e4bd22f6a53429a8
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-07 11:40:13 +0100

    Method implementation

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import instances.utils as instances_utils
 
 def get_dir(task):
     """Specifies the directory to run through, based on the task."""
@@ -7,7 +8,16 @@ def get_dir(task):
     else:
         raise ValueError("Unrecognised task")
 
-def get_method(method):
+def apply_method(method, instance):
+    """Apply the appropriate method to the example dataset."""
+    if method=='CWSavings':
+        output = cw_savings.CWSavings(instance['instance'])
+    else:
+        raise ValueError("Unrecognised method")
+
+    output.add_sol(instance['solution'])
+    output.run_all()
+    return output
 
 def avg_perf(task, method):
     """Function to run over all available instances and get the average percentage that the algorithm
@@ -17,5 +27,6 @@ def avg_perf(task, method):
     - method; the algorithm being tested"""
     directory = get_dir(task)
     for subdir in next(os.walk(directory))[1]:
-        for example in [example for example in next(os.walk(f'{directory}/{subdir}'))[2] if example.endswith('vrp')]:
-            print(example)
+        for example in [example[:-4] for example in next(os.walk(f'{directory}/{subdir}'))[2] if example.endswith('vrp')]:
+            instance = instances_utils.import_instance(subdir, example)
+            run = apply_method(method, instance)
