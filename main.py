8636d0dd86bb4dfa8802868e23dd25da3bcf297c
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-08 12:53:56 +0100

    Fix filepath

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import instances.utils as instances_utils
 import methods.cw_savings as cw_savings
 import utils
 
-data = instances_utils.import_instance('instances/A', 'A-n32-k5')
+data = instances_utils.import_instance('instances/CVRP/A', 'A-n32-k5')
 
 test = cw_savings.CWSavings(data['instance'])
 test.add_sol(data['solution'])
