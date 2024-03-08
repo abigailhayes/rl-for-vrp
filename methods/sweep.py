dc3ff0650d08c31f1014bbea370719b551e7fb4b
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-08 17:38:27 +0100

    Sorts polar coordinates, together with a reference to the node

--- methods/sweep.py
+++ methods/sweep.py
@@ -7,4 +7,7 @@ class Sweep(utils.VRPInstance):
 
     def polar_coord(self):
         depot = self.coords[0]
-        self.polars = np.append(0,np.arctan((self.coords[1:,1]-depot[1])/(self.coords[1:,0]-depot[0])))
+        polars = np.append(0,np.arctan((self.coords[1:,1]-depot[1])/(self.coords[1:,0]-depot[0])))
+        index = np.arange(polars.shape[0])  # create index array for indexing
+        polars2 = np.c_[polars, index]
+        self.polars = polars2[polars2[:, 1].argsort()]
