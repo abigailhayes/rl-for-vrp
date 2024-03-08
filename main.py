11231f21d69a0f582530e8c7b4ff3e7047cd4837
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-08 18:03:46 +0100

    Cluster based on a forward sweep

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import instances.utils as instances_utils
 import methods.cw_savings as cw_savings
+from methods.sweep import Sweep
 import utils
 
 data = instances_utils.import_instance('instances/CVRP/A', 'A-n32-k5')
 
-test = cw_savings.CWSavings(data['instance'])
+test = Sweep(data['instance'])
 test.add_sol(data['solution'])
 test.run_all()
 print(test.cost, " Perc worse: ", '{:.1%}'.format(test.perc))
--- methods/sweep.py
+++ methods/sweep.py
@@ -6,8 +6,24 @@ class Sweep(utils.VRPInstance):
     """A class for implementing Sweep on a VRP instance."""
 
     def polar_coord(self):
+        """Calculates the polar co-ordinate, and sorts with a reference to the node id."""
         depot = self.coords[0]
         polars = np.append(0,np.arctan((self.coords[1:,1]-depot[1])/(self.coords[1:,0]-depot[0])))
         index = np.arange(polars.shape[0])  # create index array for indexing
         polars2 = np.c_[polars, index]
-        self.polars = polars2[polars2[:, 1].argsort()]
+        self.polars = polars2[polars2[:, 0].argsort()]
+
+    def build_clusters(self):
+        """Build the clusters via a sweep"""
+        self.clusters = []
+        new_route = []
+        for coord, id in self.polars:
+            if id==0:
+                continue
+            new_route.append(int(id))
+            if self._cap_check(new_route) > self.capacity:
+                self.clusters.append(new_route[:-1])
+                new_route = [int(id)]
+        self.clusters.append(new_route)
+
+
