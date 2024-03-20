8f5f7bdbed654b6f4b1d8a4a7a26783c78cce5b2
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-20 17:31:00 +0100

    Add initial version of TSP nearest insertion

--- methods/TSP/utils.py
+++ methods/TSP/utils.py
@@ -24,7 +24,32 @@ class TSPInstance:
 
     def nearest_insertion(self):
         """Constructs a route based on the nearest insertion to the existing tour"""
-        route = [0]
+        route, indices = [0], [0]
+        index = 0
+        distance = self.distance.copy().view(np.ma.MaskedArray)
+        distance[:, index] = np.ma.masked
+        # Add first non-depot node, also store index, and mask in distances
+        index = distance[route].argmin()
+        route.append(self.cluster[index])
+        indices.append(index)
+        route.append(0)
+        indices.append(0)
+        distance[:, index] = np.ma.masked
+        # Loop to add in all other nodes
+        while len(route) < self.dimension+1:
+            index = np.unravel_index(distance[indices].argmin(), distance.shape)[1]
+            # Calculate the cost for adding between each pair of nodes in the current route, and add in the optimal place
+            test_list = [
+                self.distance[indices[i], index] + self.distance[index, indices[i + 1]] -
+                self.distance[indices[i], indices[i + 1]] for i in range(len(route) - 2)]
+            route.insert(np.argmin(test_list)+1, self.cluster[index])
+            indices.insert(np.argmin(test_list)+1, index)
+            distance[:, index] = np.ma.masked
+        route.remove(0)
+        route.remove(0)
+        self.route = route
+
+
 
 
 
