e95d17dd320975a2350edab2b6f84c839eb865a7
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-20 12:21:15 +0100

    Change variable naming

--- methods/TSP/utils.py
+++ methods/TSP/utils.py
@@ -11,15 +11,22 @@ class TSPInstance:
         self.route = []
 
     def nearest_neighbour(self):
+        """Constructs a route based on the nearest neighbour of the previous node"""
         self.route = []
-        curr_node = self.cluster[0]
+        curr_ind = 0
         rem_nodes = self.cluster.copy()
-        rem_nodes.remove(curr_node)
-        distance = np.delete(self.distance.copy(), curr_node, axis=1)
+        rem_nodes.remove(rem_nodes[curr_ind])
+        distance = np.delete(self.distance.copy(), rem_nodes[curr_ind], axis=1)
+        # Continue to find the nearest node to the previous node, whilst nodes remain
         while len(self.route)<self.dimension-1:
-            curr_node = distance[curr_node].argmin()
-            self.route.append(rem_nodes[curr_node])
-            rem_nodes.remove(rem_nodes[curr_node])
-            distance = np.delete(distance, curr_node, axis=1)
+            curr_ind = distance[curr_ind].argmin()
+            self.route.append(rem_nodes[curr_ind])
+            rem_nodes.remove(rem_nodes[curr_ind])
+            distance = np.delete(distance, curr_ind, axis=1)
+
+    def nearest_insertion(self):
+        """Constructs a route based on the nearest insertion to the existing tour"""
+        route = [0]
+
 
 
