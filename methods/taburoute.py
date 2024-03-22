b5f87143e4de8cc382437c65c32fb83b48e8c66c
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-22 11:55:50 +0100

    Initialisation

--- methods/taburoute.py
+++ methods/taburoute.py
@@ -1,9 +1,13 @@
-from operator import itemgetter
 import methods.utils as utils
+from math import inf
 
 
-class CWSavings(utils.VRPInstance):
+class Taburoute(utils.VRPInstance):
     """A class for implementing Clarke-Wright Savings on a VRP instance."""
 
     def __init__(self, instance):
         super().__init__(instance)
+        self.polar_coord()
+        self.alpha = 1
+        self.beta = 1
+        self.f1_best = inf
