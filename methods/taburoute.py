5dff54bf094d03027128ba5d80c3e28d0a451b69
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-25 17:04:16 +0100

    Calc p_hood in SEARCH

--- methods/taburoute.py
+++ methods/taburoute.py
@@ -1,6 +1,6 @@
 import random
 import numpy as np
-from math import inf
+from math import inf, sqrt, floor
 
 import methods.utils as utils
 from methods.TSP.genius import GENI
@@ -14,8 +14,10 @@ class Taburoute(utils.VRPInstance):
         self.polar_coord()
         self.alpha = 1
         self.p = p
-        self._init_sol(),
-        self.search_params = {'W': list(range(1,self.dimension)),
+        self.lam = floor(sqrt(self.dimension-1)/2)
+        self.f1_best = inf
+        self.f2_best = inf
+        self.search_params = {'W': list(range(1, self.dimension)),
                               'q': 5*len(self.routes),
                               'p1': None,
                               'p2': 5,
@@ -26,12 +28,22 @@ class Taburoute(utils.VRPInstance):
                               'n_max': self.dimension-1}
 
     def _f2(self):
+        """F2 evaluation function, with a penalty for an infeasible solution"""
         return self.cost + self.alpha*sum([max(0, self._cap_check(route)-self.capacity) for route in self.routes])
+
+    def _update_optimal(self, routes):
+        # Updating optimal viewed solutions if appropriate
+        if self._cap_check(routes[-1]) <= self.capacity and self._get_cost(routes) < self.f1_best:
+            self.s_best = routes  # Best feasible route so far
+            self.f1_best = self._get_cost(routes)  # Best solution so far to objective fn
+        if self._f2() < self.f2_best:
+            self.f2_best = self._f2()  # Best solution so far with penalty for infeasibility
+            self.s_best_all = routes  # Best route so far, feasible or infeasible
 
     def _init_sol(self):
         """Create initial solution by following GENI for all nodes, and then splitting to meet capacity constraints"""
         # First use GENI to give an initial solution
-        self.routes = []
+        routes = []
         i = random.randint(1, self.dimension-1)
         cluster = [0] + list(range(i, self.dimension)) + list(range(1, i))
         instance = {'cluster': cluster,
@@ -47,16 +59,18 @@ class Taburoute(utils.VRPInstance):
             new_route.append(item)
             if self._cap_check(new_route) > self.capacity:
                 # When capacity is exceeded, start new route
-                self.routes.append(new_route[:-1])
+                routes.append(new_route[:-1])
                 new_route = [item]
-        self.routes.append(new_route)  # Save final route when no more nodes
-        # Saving all info to initialise variables
-        self.get_cost()
-        if self._cap_check(self.routes[-1]) <= self.capacity:
-            self.s_best = self.routes      # Best feasible route so far
-            self.f1_best = self.cost  # Best solution so far to objective fn
-        else:
-            self.s_best = None
-            self.f1_best = inf
-        self.f2_best = self._f2()  # Best solution so far with penalty for infeasibility
-        self.s_best_all = self.routes  # Best route so far, feasible or infeasible
+        routes.append(new_route)  # Save final route when no more nodes
+
+        self._update_optimal(routes)
+        self.s = routes
+
+    def _calc_p_hood(self, node):
+        return [i for i in np.argsort(self.distance[node])[1:self.p + 1]]
+
+    def _search(self):
+        t = 1
+        candidates = random.sample(self.search_params['W'], self.search_params['q'])
+        for cand in candidates:
+            p_hood = self._calc_p_hood()
