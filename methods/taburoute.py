aa24b56efde47a972b676d294fdef425ca892e60
abigailhayes <abigail.joy.hayes@gmail.com>
2024-03-25 15:19:51 +0100

    Include p for GENI initialisation

--- methods/taburoute.py
+++ methods/taburoute.py
@@ -10,12 +10,14 @@ from methods.TSP.genius import GENI
 class Taburoute(utils.VRPInstance):
     """A class for implementing Clarke-Wright Savings on a VRP instance."""
 
-    def __init__(self, instance):
+    def __init__(self, instance, p=4):
         super().__init__(instance)
         self.polar_coord()
         self.alpha = 1
         self.beta = 1
         self.f1_best = inf
+        self.p = p
+        self._init_sol()
 
     def _init_sol(self):
         """Create initial solution by following GENI for all nodes, and then splitting to meet capacity constraints"""
@@ -26,7 +28,7 @@ class Taburoute(utils.VRPInstance):
                     'dimension': self.dimension,
                     'distance': self.distance[np.ix_(cluster, cluster)],
                     'coords': self.coords[cluster]}
-        tsp_instance = GENI(instance)
+        tsp_instance = GENI(instance, self.p)
         tsp_instance.run_all()
         single_route = tsp_instance.route
 
