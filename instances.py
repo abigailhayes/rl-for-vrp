496a12fcb656a5a82f1177e05a0d380c1ae4e495
abigailhayes <abigail.joy.hayes@gmail.com>
2024-04-10 12:28:45 +0200

    Allow for separate solution

--- instances.py
+++ instances.py
@@ -8,29 +8,29 @@ data = instances_utils.import_instance('instances/CVRP/A', 'A-n80-k10')
 
 
 # Adapted from https://alns.readthedocs.io/en/stable/examples/capacitated_vehicle_routing_problem.html
-def plot_solution(instance, name="CVRP solution"):
+def plot_solution(instance, solution, name="CVRP solution"):
     """
     Plot the routes of the passed-in solution.
     """
     fig, ax = plt.subplots(figsize=(12, 10))
-    cmap = matplotlib.cm.rainbow(np.linspace(0, 1, len(instance['solution']['routes'])))
+    cmap = matplotlib.cm.rainbow(np.linspace(0, 1, len(solution['routes'])))
 
-    for idx, route in enumerate(instance['solution']['routes']):
+    for idx, route in enumerate(solution['routes']):
         ax.plot(
-            [instance['instance']["node_coord"][loc][0] for loc in [0] + route + [0]],
-            [instance['instance']["node_coord"][loc][1] for loc in [0] + route + [0]],
+            [instance["node_coord"][loc][0] for loc in [0] + route + [0]],
+            [instance["node_coord"][loc][1] for loc in [0] + route + [0]],
             color=cmap[idx],
             marker='.'
         )
 
     # Plot the depot
     kwargs = dict(label="Depot", zorder=3, marker="*", s=750)
-    ax.scatter(data['instance']["node_coord"][0][0], data['instance']["node_coord"][0][1], c="tab:red", **kwargs)
+    ax.scatter(instance["node_coord"][0][0], instance["node_coord"][0][1], c="tab:red", **kwargs)
 
-    ax.set_title(f"{name}\n Total distance: {data['solution']['cost']}")
+    ax.set_title(f"{name}\n Total distance: {solution['cost']}")
     ax.set_xlabel("X-coordinate")
     ax.set_ylabel("Y-coordinate")
     ax.legend(frameon=False, ncol=3)
 
 
-plot_solution(data, name="Best known solution")
+plot_solution(data['instance'], data['solution'], name="Best known solution")
