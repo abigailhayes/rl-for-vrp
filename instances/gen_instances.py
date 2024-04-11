102625064e77922788667e55dc02efddee27756d
abigailhayes <abigail.joy.hayes@gmail.com>
2024-04-11 09:48:11 +0200

    Add explanation of gen_cvrp fn

--- instances/gen_instances.py
+++ instances/gen_instances.py
@@ -1,29 +1,31 @@
 import random
 import numpy as np
+import vrplib
 
-def gen_cvrp(type, nodes, capacity, max_demand):
+
+def gen_cvrp(filepath, nodes, capacity, max_demand, node_type='random', depot_type='centre'):
     # Initial values
-    output = {'name': f'{type}-{nodes}-{capacity}-{max_demand}',
+    output = {'name': f'{node_type}-{depot_type}-{nodes}-{capacity}-{max_demand}',
               'type': 'CVRP',
-              'dimension': nodes+1,
+              'dimension': nodes + 1,
               'edge_weight_type': 'EUC_2D',
               'capacity': capacity}
 
     # Helper function for generating coordinates
     def random_coords():
-        return [random.random() for i in range(2)]
+        return [random.random() for _ in range(2)]
 
     # Set depot location and demand
-    if type == 'random_centre':
-        coords = [[0.5,0.5]]
-    elif type == 'random_random':
+    if depot_type == 'centre':
+        coords = [[0.5, 0.5]]
+    elif depot_type == 'random':
         coords = [random_coords()]
     demand = [0]
 
     # Node co-ordinates and demand
     for i in range(nodes):
         coords.append(random_coords())
-        demand.append(random.randint(1,max_demand))
+        demand.append(random.randint(1, max_demand))
     output['node_coord'] = np.array(coords)
     output['demand'] = np.array(demand)
 
@@ -31,19 +33,9 @@ def gen_cvrp(type, nodes, capacity, max_demand):
     output['depot'] = [0]
 
     # Edge weight (distances)
-    distances = np.zeros((nodes+1, nodes+1))
-    for i in range(nodes+1):
-        distances[i] = np.linalg.norm(output['node_coord'] - output['node_coord'][i])
+    distances = np.zeros((nodes + 1, nodes + 1))
+    for i in range(nodes + 1):
+        distances[i] = np.linalg.norm(output['node_coord'] - output['node_coord'][i], axis=1)
     output['edge_weight'] = distances
 
-    return output
-
-# name type-nodes-capacity-max_demand
-# type CVRP
-# dimension
-# edge_weight_type
-# capacity
-# node_coord
-# demand
-# depot [0]
-# edge_weight
+    vrplib.write_instance(f"{filepath}/{output['name']}", output)
