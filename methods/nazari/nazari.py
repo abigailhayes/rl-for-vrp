4d892abaaffee2cb8c3c588d4c97f8806dbd3524
abigailhayes <abigail.joy.hayes@gmail.com>
2024-04-16 10:44:46 +0200

    Instance format conversion

--- methods/nazari/nazari.py
+++ methods/nazari/nazari.py
@@ -4,14 +4,14 @@ import vrplib
 import os
 
 # Data formatting
-directory = 'instances/CVRP/generate/random_all-20-normal-normal-1'
+directory = 'instances/CVRP/generate/random_random-20-normal-normal-1'
 working = []
 for file in next(os.walk(directory))[2]:
     if os.path.splitext(file)[-1].lower() == '.vrp':
         print('Running:', file)
         instance = vrplib.read_instance(f'{directory}/{file}')
-        result = np.concatenate((instance['node_coord'], instance['demand']), axis=1)
-        np.roll(result, -1) # Move the depot to the end
+        result = np.column_stack((instance['node_coord'], instance['demand']))
+        result = np.roll(result, -1, axis=0) # Move the depot to the end
         working.append(result)
 output = np.stack(working, axis=0)
 
