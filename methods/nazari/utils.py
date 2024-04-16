b416d6d525c702b5243858639e82bac6f17a5fc4
abigailhayes <abigail.joy.hayes@gmail.com>
2024-04-16 15:29:07 +0200

    Reverted to original code version, and have it running

--- methods/nazari/utils.py
+++ methods/nazari/utils.py
@@ -418,7 +418,9 @@ class DataGenerator(object):
 
         # create test data
         self.n_problems = args['test_size']
-        self.test_data = args['test_data']
+        # self.test_data = args['test_data']
+        self.test_data = create_VRP_dataset(self.n_problems, args['n_cust'], './data',
+                                            seed=args['random_seed'] + 1, data_type='test')
 
         self.reset()
 
