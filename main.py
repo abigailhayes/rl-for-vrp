4d3bd148cf7f9433b3972673b508e40dd0d16e07
abigailhayes <abigail.joy.hayes@gmail.com>
2024-04-17 16:58:46 +0200

    Set up experiment ids

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ import utils
 
 def main():
     args = utils.parse_experiment()
+
+    # Looking at current ids in use as folders
+    id_list = [int(str.replace(item, 'exp_', '')) for item in os.listdir('results') if 'run' in item]
+
+    # determine ID of this run
+    if len(id_list) == 0:
+        id = 1
+    else:
+        id = max(id_list) + 1
