0f2b8c135caabcead9d913fa2616a4abac99403e
abigailhayes <abigail.joy.hayes@gmail.com>
2024-04-19 10:38:00 +0200

    Instance generation to only use numbers

--- instances/gen_instances.py
+++ instances/gen_instances.py
@@ -62,51 +62,35 @@ def gen_cvrp(filepath, ident, nodes, capacity, max_demand, node_type='random', d
     vrplib.write_instance(f"{filepath}/{output['NAME']}.vrp", output)
 
 
-def gen_cvrp_multi(set_type, seed, number=100, nodes=20, capacity='normal', demand='normal'):
+def gen_cvrp_multi(set_type, seed, number=100, nodes=20, capacity=100, max_demand=20):
     """Setting up to generate instances with specific characteristics
     - set_type - the name used to refer overall to the type
     - seed - generation will be reproducible
     - number - how many instances to generate
     - nodes - number of nodes in each instance
-    - capacity - defaults to being 100, but can be set to high or low
-    - demand - specifies how the maximum demand relates to the capacity of each vehicle"""
+    - capacity - defaults to being 100, but can be set to other values
+    - max_demand - specifies the maximum demand"""
     # Set up folder
-    filepath = f'instances/CVRP/generate/{set_type}-{nodes}-{demand}-{capacity}-{seed}'
+    filepath = f'instances/CVRP/generate/{set_type}-{nodes}-{max_demand}-{capacity}-{seed}'
     os.makedirs(filepath, exist_ok=True)
     random.seed(seed)
-
-    # Set capacity
-    if capacity == 'normal':
-        capacity_n = 100
-    elif capacity == 'low':
-        capacity_n = 20
-    elif capacity == 'high':
-        capacity_n = 250
-
-    # Allow for demand
-    if demand == 'normal':
-        max_demand = round(capacity_n/5)
-    elif demand == 'high':
-        max_demand = round(capacity_n/2)
-    elif demand == 'low':
-        max_demand = round(capacity_n/15)
 
     # Generate instances
     if set_type == 'random_random':
         for i in range(number):
-            gen_cvrp(filepath, i, nodes, capacity_n, max_demand, node_type='random', depot_type='random')
+            gen_cvrp(filepath, i, nodes, capacity, max_demand, node_type='random', depot_type='random')
     elif set_type == 'random_centre':
         for i in range(number):
-            gen_cvrp(filepath, i, nodes, capacity_n, max_demand, node_type='random', depot_type='centre')
+            gen_cvrp(filepath, i, nodes, capacity, max_demand, node_type='random', depot_type='centre')
     elif set_type == 'random_outer':
         for i in range(number):
-            gen_cvrp(filepath, i, nodes, capacity_n, max_demand, node_type='random', depot_type='outer')
+            gen_cvrp(filepath, i, nodes, capacity, max_demand, node_type='random', depot_type='outer')
     elif set_type == 'cluster_random':
         for i in range(number):
-            gen_cvrp(filepath, i, nodes, capacity_n, max_demand, node_type='clusters', depot_type='random')
+            gen_cvrp(filepath, i, nodes, capacity, max_demand, node_type='clusters', depot_type='random')
     elif set_type == 'cluster_centre':
         for i in range(number):
-            gen_cvrp(filepath, i, nodes, capacity_n, max_demand, node_type='clusters', depot_type='centre')
+            gen_cvrp(filepath, i, nodes, capacity, max_demand, node_type='clusters', depot_type='centre')
     elif set_type == 'cluster_outer':
         for i in range(number):
-            gen_cvrp(filepath, i, nodes, capacity_n, max_demand, node_type='clusters', depot_type='outer')
+            gen_cvrp(filepath, i, nodes, capacity, max_demand, node_type='clusters', depot_type='outer')
