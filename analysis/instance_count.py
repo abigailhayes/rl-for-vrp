2dbbdac01d0d5923855530d06584ec9bd597fb8c
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-06 11:16:52 +0200

    Switch into main()

--- analysis/instance_count.py
+++ analysis/instance_count.py
@@ -2,27 +2,31 @@ import json
 import pandas as pd
 
 
-# Load in current counts
-settings_df = pd.read_csv('results/settings.csv')
-instances_df = pd.read_csv('results/instance_count.csv')
+def main():
+    # Load in current counts
+    settings_df = pd.read_csv('results/settings.csv')
+    instances_df = pd.read_csv('results/instance_count.csv')
 
 
-# Update dataframe
-def instance_row(ident):
-    output = {'id': ident}
-    json_paths = [f'results/exp_{ident}/results_a.json', f'results/exp_{ident}/results_b.json']
-    for json_path in json_paths:
-        with open(json_path) as json_data:
-            data = json.load(json_data)
-        for key in data:
-            output[key] = len(data[key])
-    return pd.DataFrame.from_dict([output])
+    # Update dataframe
+    def instance_row(ident):
+        output = {'id': ident}
+        json_paths = [f'results/exp_{ident}/results_a.json', f'results/exp_{ident}/results_b.json']
+        for json_path in json_paths:
+            with open(json_path) as json_data:
+                data = json.load(json_data)
+            for key in data:
+                output[key] = len(data[key])
+        return pd.DataFrame.from_dict([output])
 
 
-# Run over new ids
-targets = [ident for ident in settings_df['id'] if ident not in instances_df['id']]
-for ident in targets:
-    instances_df = pd.concat([instances_df, instance_row(ident)], ignore_index=True)
+    # Run over new ids
+    targets = [ident for ident in settings_df['id'] if ident not in instances_df['id']]
+    for ident in targets:
+        instances_df = pd.concat([instances_df, instance_row(ident)], ignore_index=True)
 
-# Save output
-instances_df.to_csv('results/instance_count.csv', index=False)
+    # Save output
+    instances_df.to_csv('results/instance_count.csv', index=False)
+
+if __name__ == '__main__':
+    main()
