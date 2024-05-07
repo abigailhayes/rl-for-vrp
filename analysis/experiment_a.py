56d1f353864cdf6cc0da109d957ebcb572e1f212
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-07 11:45:39 +0200

    Calculate percentage differences

--- analysis/experiment_a.py
+++ analysis/experiment_a.py
@@ -4,9 +4,31 @@ import json
 
 import analysis.utils as analysis_utils
 
+
 def compare_optimum(exp_filepath):
     # Load in data
     optima = analysis_utils.baseline_optima()
     with open(exp_filepath) as json_data:
         results = json.load(json_data)
 
+    comparison = {}
+    for test_set in optima:
+        comparison[test_set] = {}
+        for instance in test_set:
+            if instance in results[test_set] and results[test_set][instance] is dict:
+                # Compiled OR tools results format
+                comparison[test_set][instance] = (results[test_set][instance]['value'] - optima[test_set][
+                    instance]) / optima[test_set][instance]
+            elif instance in results[test_set]:
+                # General results format
+                comparison[test_set][instance] = (results[test_set][instance] - optima[test_set][instance]) / optima[
+                    test_set][instance]
+            else:
+                # Nazari format
+                comparison[test_set][instance] = {}
+                comparison[test_set][instance]['greedy'] = (results[test_set]['greedy'][instance] - optima[test_set][
+                    instance]) / optima[test_set][instance]
+                comparison[test_set][instance]['beam'] = (results[test_set]['beam'][instance] - optima[test_set][
+                    instance]) / optima[test_set][instance]
+
+    return comparison
