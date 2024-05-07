ea9e5833196281ba9f1b6d47fa8e95c104a3eee7
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-07 10:17:52 +0200

    Calculate baseline optima

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,13 +1,26 @@
 import os
+import json
 from pathlib import Path
-
-import vrplib
+from itertools import pairwise
 
 import instances.utils as instance_utils
 
-test_sets = ['A', 'B', 'E', 'F', 'M', 'P', 'CMT']
-for test_set in test_sets:
-    for instance in next(os.walk(f'instances/CVRP/{test_set}'))[2]:
-        if instance.endswith('sol'):
-            continue
-        data = instance_utils.import_instance(f'instances/CVRP/{test_set}', Path(instance).stem)
+
+def baseline_optima():
+    test_sets = ['A', 'B', 'E', 'F', 'M', 'P', 'CMT']
+    output = {}
+    for test_set in test_sets:
+        output[test_set] = {}
+        for instance in next(os.walk(f'instances/CVRP/{test_set}'))[2]:
+            if instance.endswith('sol'):
+                continue
+            data = instance_utils.import_instance(f'instances/CVRP/{test_set}', Path(instance).stem)
+            costs = 0
+            for r in data['solution']['routes']:
+                for i, j in pairwise([0] + r + [0]):
+                    costs += data['instance']['edge_weight'][i][j]
+            output[test_set][instance] = costs
+
+    # Save output
+    with open(f'results/baseline_optima.json', 'w') as f:
+        json.dump(output, f, indent=2)
