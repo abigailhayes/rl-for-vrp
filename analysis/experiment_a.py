6a3323ccf78aa2b61fd9843dfb703e65e435f75f
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-07 11:48:08 +0200

    Add comments and error handling

--- analysis/experiment_a.py
+++ analysis/experiment_a.py
@@ -6,29 +6,34 @@ import analysis.utils as analysis_utils
 
 
 def compare_optimum(exp_filepath):
+    """Compare a results file to the optimal baseline solutions."""
     # Load in data
     optima = analysis_utils.baseline_optima()
     with open(exp_filepath) as json_data:
         results = json.load(json_data)
 
+    # Compare for each instance
     comparison = {}
     for test_set in optima:
         comparison[test_set] = {}
         for instance in test_set:
-            if instance in results[test_set] and results[test_set][instance] is dict:
-                # Compiled OR tools results format
-                comparison[test_set][instance] = (results[test_set][instance]['value'] - optima[test_set][
-                    instance]) / optima[test_set][instance]
-            elif instance in results[test_set]:
-                # General results format
-                comparison[test_set][instance] = (results[test_set][instance] - optima[test_set][instance]) / optima[
-                    test_set][instance]
-            else:
-                # Nazari format
-                comparison[test_set][instance] = {}
-                comparison[test_set][instance]['greedy'] = (results[test_set]['greedy'][instance] - optima[test_set][
-                    instance]) / optima[test_set][instance]
-                comparison[test_set][instance]['beam'] = (results[test_set]['beam'][instance] - optima[test_set][
-                    instance]) / optima[test_set][instance]
+            try:
+                if instance in results[test_set] and results[test_set][instance] is dict:
+                    # Compiled OR tools results format
+                    comparison[test_set][instance] = (results[test_set][instance]['value'] - optima[test_set][
+                        instance]) / optima[test_set][instance]
+                elif instance in results[test_set]:
+                    # General results format
+                    comparison[test_set][instance] = (results[test_set][instance] - optima[test_set][
+                        instance]) / optima[test_set][instance]
+                else:
+                    # Nazari format
+                    comparison[test_set][instance] = {}
+                    comparison[test_set][instance]['greedy'] = (results[test_set]['greedy'][instance] - optima[
+                        test_set][instance]) / optima[test_set][instance]
+                    comparison[test_set][instance]['beam'] = (results[test_set]['beam'][instance] - optima[test_set][
+                        instance]) / optima[test_set][instance]
+            except NameError:
+                pass
 
     return comparison
