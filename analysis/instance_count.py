2577f8db6e8e0dce937315d3f07308f27a325b96
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-08 15:45:18 +0200

    Set up Nazari instance count

--- analysis/instance_count.py
+++ analysis/instance_count.py
@@ -8,21 +8,33 @@ def main():
     settings_df = pd.read_csv('results/settings.csv')
     instances_df = pd.read_csv('results/instance_count.csv')
 
-
     # Update dataframe
     def instance_row(ident):
         """Count the instances for a given id"""
-        output = {'id': ident}
         json_paths = [f'results/exp_{ident}/results_a.json', f'results/exp_{ident}/results_b.json']
-        try:
+        if settings_df[settings_df['id']==ident]['method']=='nazari':
+            # When data is split between beam and greedy
+            output_greedy = {'id': ident, 'notes': 'greedy'}
+            output_beam = {'id': ident, 'notes': 'beam'}
             for json_path in json_paths:
                 with open(json_path) as json_data:
                     data = json.load(json_data)
                 for key in data:
-                    output[key] = len(data[key])
-        except ValueError:
-            pass
-        return pd.DataFrame.from_dict([output])
+                    output_greedy[key] = len(data['greedy'][key])
+                    output_beam[key] = len(data['beam'][key])
+            return pd.concat([pd.DataFrame.from_dict([output_greedy]), pd.DataFrame.from_dict([output_beam])], ignore_index=True)
+        else:
+            # When data is stored directly for each instance
+            for json_path in json_paths:
+                try:
+                    output = {'id': ident}
+                    with open(json_path) as json_data:
+                        data = json.load(json_data)
+                    for key in data:
+                        output[key] = len(data[key])
+                except ValueError:
+                    pass
+            return pd.DataFrame.from_dict([output])
 
 
     # Run over new ids
@@ -32,6 +44,7 @@ def main():
 
     # Save output
     instances_df.to_csv('results/instance_count.csv', index=False)
+
 
 if __name__ == '__main__':
     main()
