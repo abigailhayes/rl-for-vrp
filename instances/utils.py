08f067c6da44b8bed69a8fb97645e909c830deb5
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-14 15:48:46 +0200

    Function to derive smaller Solomon instances

--- instances/utils.py
+++ instances/utils.py
@@ -13,3 +13,15 @@ def import_twinstance(folder, name):
     output = {'instance': vrplib.read_instance(f'{folder}/{name}.txt', instance_format="solomon"),
               'solution': vrplib.read_solution(f'{folder}/{name}.sol')}
     return output
+
+
+def shrink_twinstance(instance, size):
+    """Deriving the smaller Solomon instances"""
+    return {'name': instance['name']+'.'+str(size),
+            'vehicles': instance['vehicles'],
+            'capacity': instance['capacity'],
+            'node_coord': instance['node_coord'][:size+1,],
+            'demand': instance['demand'][:size+1,],
+            'time_window': instance['time_window'][:size+1,],
+            'service_time': instance['service_time'][:size+1,],
+            'edge_weight': instance['edge_weight'][:size+1,:size+1]}
