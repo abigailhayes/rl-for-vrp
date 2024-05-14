7fa66ea3cebff481c1779679052fecaf20fa8b48
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-14 15:38:58 +0200

    Reading in instances in Solomon format

--- instances/utils.py
+++ instances/utils.py
@@ -2,6 +2,14 @@ import vrplib
 
 
 def import_instance(folder, name):
+    """Import standard CVRP instance, in VRPLIB format"""
     output = {'instance': vrplib.read_instance(f'{folder}/{name}.vrp'),
+              'solution': vrplib.read_solution(f'{folder}/{name}.sol')}
+    return output
+
+
+def import_twinstance(folder, name):
+    """Import CVRP-TW instance, in Solomon format"""
+    output = {'instance': vrplib.read_instance(f'{folder}/{name}.txt', instance_format="solomon"),
               'solution': vrplib.read_solution(f'{folder}/{name}.sol')}
     return output
