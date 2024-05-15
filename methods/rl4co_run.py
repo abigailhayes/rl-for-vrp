eb1a07fefcf347d69c7eb65715d0a8a38f10b865
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-15 16:16:24 +0200

    Handling package issues

--- methods/rl4co_run.py
+++ methods/rl4co_run.py
@@ -22,7 +22,7 @@ class RL4CO:
         seed_everything(seed, workers=True)
         self.init_method = init_method
         self.customers = customers
-        self.env = CVRPEnv(generator_params=dict(num_loc=self.customers))
+        self.env = CVRPEnv(num_loc=self.customers)
 
     def set_model(self):
         if self.init_method == 'am':
