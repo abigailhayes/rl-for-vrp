93b8be8afcf510978fe1e8bb18bdd01aaa8b565d
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-23 16:41:13 +0200

    Adding in additional CVRPTW variables for testing

--- methods/rl4co_run.py
+++ methods/rl4co_run.py
@@ -22,51 +22,66 @@ class RL4CO:
         seed_everything(seed, workers=True)
         self.init_method = init_method
         self.customers = customers
-        if problem == 'CVRP':
+        self.problem = problem
+        if self.problem == "CVRP":
             self.env = CVRPEnv(num_loc=self.customers)
-        elif problem =='CVRPTW':
+        elif self.problem == "CVRPTW":
             self.env = CVRPTWEnv(num_loc=self.customers)
 
     def set_model(self):
-        if self.init_method == 'am':
-            self.model = AttentionModel(self.env,
-                                        train_data_size=250_000,
-                                        test_data_size=10_000,
-                                        optimizer_kwargs={'lr': 1e-4})
-        elif self.init_method == 'amppo':
-            self.model = AMPPO(self.env,
-                               train_data_size=250_000,
-                               test_data_size=10_000,
-                               optimizer_kwargs={'lr': 1e-4})
-        elif self.init_method == 'symnco':
-            self.model = SymNCO(self.env,
-                                train_data_size=250_000,
-                                test_data_size=10_000,
-                                optimizer_kwargs={'lr': 1e-4})
-        elif self.init_method == 'pomo':
-            self.model = POMO(self.env,
-                              train_data_size=100_000,
-                              test_data_size=10_000,
-                              optimizer_kwargs={'lr': 1e-4})
-        elif self.init_method == 'mdam':
-            self.model = MDAM(self.env,
-                              train_data_size=250_000,
-                              test_data_size=10_000,
-                              optimizer_kwargs={'lr': 1e-4})
-        elif self.init_method == 'deepaco':
-            self.model = DeepACO(self.env,
-                                 train_data_size=640,
-                                 test_data_size=320,
-                                 optimizer_kwargs={'lr': 1e-4})
+        if self.init_method == "am":
+            self.model = AttentionModel(
+                self.env,
+                train_data_size=250_000,
+                test_data_size=10_000,
+                optimizer_kwargs={"lr": 1e-4},
+            )
+        elif self.init_method == "amppo":
+            self.model = AMPPO(
+                self.env,
+                train_data_size=250_000,
+                test_data_size=10_000,
+                optimizer_kwargs={"lr": 1e-4},
+            )
+        elif self.init_method == "symnco":
+            self.model = SymNCO(
+                self.env,
+                train_data_size=250_000,
+                test_data_size=10_000,
+                optimizer_kwargs={"lr": 1e-4},
+            )
+        elif self.init_method == "pomo":
+            self.model = POMO(
+                self.env,
+                train_data_size=100_000,
+                test_data_size=10_000,
+                optimizer_kwargs={"lr": 1e-4},
+            )
+        elif self.init_method == "mdam":
+            self.model = MDAM(
+                self.env,
+                train_data_size=250_000,
+                test_data_size=10_000,
+                optimizer_kwargs={"lr": 1e-4},
+            )
+        elif self.init_method == "deepaco":
+            self.model = DeepACO(
+                self.env,
+                train_data_size=640,
+                test_data_size=320,
+                optimizer_kwargs={"lr": 1e-4},
+            )
 
     def train_model(self):
-        if self.init_method == 'deepaco':
+        if self.init_method == "deepaco":
             epochs = 1
         else:
             epochs = 100
             # Currently ignoring POMO instructions for 2000 epochs
-        trainer_kwargs = {'accelerator': "auto",
-                          'default_root_dir': f'results/exp_{self.ident}'}
+        trainer_kwargs = {
+            "accelerator": "auto",
+            "default_root_dir": f"results/exp_{self.ident}",
+        }
         self.trainer = RL4COTrainer(max_epochs=epochs, **trainer_kwargs)
         self.trainer.fit(self.model)
 
@@ -86,7 +101,7 @@ class RL4CO:
         # Routing
         self.routes = []
         current = []
-        for node in out['actions'][0]:
+        for node in out["actions"][0]:
             if node == 0:
                 self.routes.append(current)
                 current = []
@@ -96,11 +111,14 @@ class RL4CO:
         self.routes = [route for route in self.routes if len(route) != 0]
 
     def single_test(self, instance):
-        """ Test for a single instance """
-        coords = torch.tensor(instance['node_coord']).float()
+        """Test for a single instance"""
+        coords = torch.tensor(instance["node_coord"]).float()
         coords_norm = self.normalize_coord(coords)
-        demand = torch.tensor(instance['demand'][1:]).float()
-        capacity = instance['capacity']
+        demand = torch.tensor(instance["demand"][1:]).float()
+        capacity = instance["capacity"]
+        if self.problem == "CVRPTW":
+            durations = torch.tensor(instance["service_time"][1:]).float()
+            time_windows = torch.tensor(instance["time_window"][1:]).float()
         n = coords.shape[0]
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         policy = self.model.policy
@@ -109,21 +127,23 @@ class RL4CO:
         # Prepare the tensordict
         batch_size = 2
         td = self.env.reset(batch_size=(batch_size,)).to(device)
-        td['locs'] = repeat(coords_norm, 'n d -> b n d', b=batch_size, d=2)
-        td['demand'] = repeat(demand, 'n -> b n', b=batch_size) / capacity
-        td['visited'] = torch.zeros((batch_size, 1, n), dtype=torch.uint8)
+        td["locs"] = repeat(coords_norm, "n d -> b n d", b=batch_size, d=2)
+        td["demand"] = repeat(demand, "n -> b n", b=batch_size) / capacity
+        td["visited"] = torch.zeros((batch_size, 1, n), dtype=torch.uint8)
+        if self.problem == "CVRPTW":
+            td["durations"] = repeat(durations, "n -> b n", b=batch_size)
+            td["time_windows"] = repeat(time_windows, "n -> b n d", b=batch_size, d=2)
         action_mask = torch.ones(batch_size, n, dtype=torch.bool)
         action_mask[:, 0] = False
-        td['action_mask'] = action_mask
+        td["action_mask"] = action_mask
 
         # Get the solution from the policy
         with torch.no_grad():
-            out = policy(td.clone(), decode_type='greedy', return_actions=True)
+            out = policy(td.clone(), decode_type="greedy", return_actions=True)
 
         # Calculate the cost on the original scale
-        td['locs'] = repeat(coords, 'n d -> b n d', b=batch_size, d=2)
-        neg_reward = self.env.get_reward(td, out['actions'])
+        td["locs"] = repeat(coords, "n d -> b n d", b=batch_size, d=2)
+        neg_reward = self.env.get_reward(td, out["actions"])
         self.cost = ceil(-1 * neg_reward[0].item())
 
         self.routing()
-
