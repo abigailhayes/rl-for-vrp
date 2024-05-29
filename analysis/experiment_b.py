58cc2d181ea48206f36c2acaea73cf1e09471b68
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-29 17:30:35 +0200

    Set up for standard setting

--- analysis/experiment_b.py
+++ analysis/experiment_b.py
@@ -1,23 +1,39 @@
 import json
 import pandas as pd
 
-settings_df = pd.read_csv('results/settings.csv')
-instance_count = pd.read_csv('results/instance_count.csv')
+# TODO - ensure limited to CVRP
 
-col_name = 'cluster_centre-20-30-100-42'
+settings_df = pd.read_csv("results/settings.csv")
+instance_count = pd.read_csv("results/instance_count.csv")
 
 
-def ids_for_col(col_name):
-    """Gets just the ids with solutions for all instances"""
-    ids = instance_count[instance_count['id'] != 0][instance_count[instance_count['id'] != 0][col_name] == max(instance_count[instance_count['id'] != 0][col_name])][['id', 'notes']]
-    return ids
+def check_instances(col_name):
+    """Boolean indication of whether the full instance set is evaluated"""
+    output = include[col_name] == max(include[col_name])
+    return output
 
-def avg_for_col(col_name):
-    ids = ids_for_col(col_name)
 
-    for _, row in ids.iterrows():
-        if pd.isna(row['notes']):
-            # Load in certain way
-        elif row['notes'] in ['greedy', 'beam']:
-            # Load in another way
+def average_distance(folder_dict):
+    output = []
+    for key in folder_dict:
+        output.append(folder_dict[key])
+    return sum(output)/len(output)
 
+
+include = instance_count.drop(["id", "notes"], axis=1)
+include = include.drop(index=0, axis=0)
+for column_name in list(include):
+    include[column_name] = check_instances(column_name)
+include["id"] = instance_count["id"]
+include["notes"] = instance_count["notes"]
+for index, row in include.iterrows():
+    if row["id"] > 10:  # FOR TESTING
+        continue
+    with open(f'results/exp_{row["id"]}/results_b.json') as json_data:
+        data = json.load(json_data)
+    if pd.isna(row["notes"]):
+        for key in data:
+            if row[key]:
+                include[key][index] = average_distance(data[key])
+    elif row["notes"] in ["greedy", "beam"]:
+        print(row)
