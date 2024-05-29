c0fe79deeab8c280e5b58a1c854aa35bfa7c1c26
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-29 18:18:31 +0200

    Set up for saving expt B means

--- analysis/experiment_b.py
+++ analysis/experiment_b.py
@@ -1,39 +1,55 @@
 import json
 import pandas as pd
 
-# TODO - ensure limited to CVRP
 
-settings_df = pd.read_csv("results/settings.csv")
-instance_count = pd.read_csv("results/instance_count.csv")
-
-
-def check_instances(col_name):
-    """Boolean indication of whether the full instance set is evaluated"""
-    output = include[col_name] == max(include[col_name])
-    return output
+def check_instances(table, col_name: str):
+    """Boolean indication of whether the full instance set is evaluated, returned as float"""
+    output = table[col_name] == max(table[col_name])
+    return output * 1.0
 
 
-def average_distance(folder_dict):
+def average_distance(folder_dict: dict):
+    """Given a dictionary with entries for each instance, takes the mean of the values"""
     output = []
     for key in folder_dict:
         output.append(folder_dict[key])
-    return sum(output)/len(output)
+    return sum(output) / len(output)
 
 
-include = instance_count.drop(["id", "notes"], axis=1)
-include = include.drop(index=0, axis=0)
-for column_name in list(include):
-    include[column_name] = check_instances(column_name)
-include["id"] = instance_count["id"]
-include["notes"] = instance_count["notes"]
-for index, row in include.iterrows():
-    if row["id"] > 10:  # FOR TESTING
-        continue
-    with open(f'results/exp_{row["id"]}/results_b.json') as json_data:
-        data = json.load(json_data)
-    if pd.isna(row["notes"]):
-        for key in data:
-            if row[key]:
-                include[key][index] = average_distance(data[key])
-    elif row["notes"] in ["greedy", "beam"]:
-        print(row)
+def b_all_averages():
+    """Get the averages for all experiment B instance types"""
+    instance_count = pd.read_csv("results/instance_count.csv")
+
+    # Get a dataframe showing where averages should be taken
+    include = instance_count.drop(
+        ["A", "B", "E", "F", "M", "P", "CMT", "id", "notes"], axis=1
+    )
+    include = include.drop(index=0, axis=0)
+    for column_name in list(include):
+        include[column_name] = check_instances(column_name)
+    include["id"] = instance_count["id"]
+    include["notes"] = instance_count["notes"]
+
+    # Now go through and get averages
+    for index, row in include.iterrows():
+        if row["id"] > 10:  # FOR TESTING
+            continue
+        with open(f'results/exp_{row["id"]}/results_b.json') as json_data:
+            data = json.load(json_data)
+        if pd.isna(row["notes"]):
+            for key in data:
+                if row[key] == 1:
+                    include.loc[index, key] = average_distance(data[key])
+        elif row["notes"] in ["greedy", "beam"]:
+            for key in data:
+                if row[key] == 1:
+                    include.loc[index, key] = average_distance(data[key][row["notes"]])
+
+    include.to_csv("results/expt_b_means.csv", index=False)
+
+
+def main():
+    b_all_averages()
+
+if __name__ == '__main__':
+    main()
