59376cb6cb7d221e063228a583b4e6388e2bf34a
abigailhayes <abigail.joy.hayes@gmail.com>
2024-05-29 11:18:42 +0200

    50 to 20 epochs for timeliness

--- methods/rl4co_run.py
+++ methods/rl4co_run.py
@@ -76,7 +76,7 @@ class RL4CO:
         if self.init_method == "deepaco":
             epochs = 1
         else:
-            epochs = 50
+            epochs = 20
             # Currently ignoring POMO instructions for 2000 epochs
         trainer_kwargs = {
             "accelerator": "auto",
