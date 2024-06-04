16fdcb8bf8aa9d52051bec12ec68ee53edc5f630
abigailhayes <abigail.joy.hayes@gmail.com>
2024-06-04 10:29:46 +0200

    Look for exp_ instead of exp for expt number

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main():
     args = utils.parse_experiment()
 
     # Looking at current ids in use as folders
-    id_list = [int(str.replace(item, 'exp_', '')) for item in os.listdir('results') if 'exp' in item]
+    id_list = [int(str.replace(item, 'exp_', '')) for item in os.listdir('results') if 'exp_' in item]
 
     # Determine ID of this run
     if len(id_list) == 0:
