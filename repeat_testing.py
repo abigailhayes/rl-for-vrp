cb6e6e8cdcb628a555f255ed15194f0e48a84db0
abigailhayes <abigail.joy.hayes@gmail.com>
2024-07-06 12:07:48 +0200

    Model loading for additional testing

---
+++ repeat_testing.py
@@ -0,0 +1,50 @@
+import argparse
+import pandas as pd
+import os
+
+import methods.rl4co_run as rl4co_run
+
+
+def parse_run():
+    # To handle dictionary input
+    class ParseKwargs(argparse.Action):
+        def __call__(self, parser, namespace, values, option_string=None):
+            setattr(namespace, self.dest, dict())
+            for value in values:
+                key, value = value.split("=")
+                getattr(namespace, self.dest)[key] = value
+
+    """Parse arguments for an experiment run"""
+    parser = argparse.ArgumentParser(description="Experiment arguments")
+    parser.add_argument("--id", help="Specify random seed")
+
+    args, unknown = parser.parse_known_args()
+    args = vars(args)
+
+    return args
+
+
+def main():
+    args = parse_run()
+    settings_df = pd.read_csv("results/other/settings.csv").to_dict("index")
+    settings = settings_df[int(args["id"])]
+
+    # Load in model
+    if settings["method"] == "rl4co":
+        model = rl4co_run.RL4CO(
+            settings["problem"],
+            settings["init_method"],
+            settings["customers"],
+            settings["id"],
+            args["id"],
+        )
+        model.set_model()
+        root_path = f"results/exp_{args['id']}/lightning_logs"
+        long_path = f"{root_path}/{os.listdir(root_path)[0]}/checkpoints"
+        model.model.load_from_checkpoint(
+            f"{long_path}/{os.listdir(long_path)[0]}"
+        )
+
+
+if __name__ == "__main__":
+    main()
