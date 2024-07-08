7f662a37100a772d13c7f2db571ac43d3f24152d
abigailhayes <abigail.joy.hayes@gmail.com>
2024-07-08 17:05:44 +0200

    Set up repeated testing

--- repeat_testing.py
+++ repeat_testing.py
@@ -1,8 +1,13 @@
 import argparse
 import pandas as pd
 import os
+import ast
+import json
+
+from lightning.pytorch import seed_everything
 
 import methods.rl4co_run as rl4co_run
+import utils
 
 
 def parse_run():
@@ -16,7 +21,10 @@ def parse_run():
 
     """Parse arguments for an experiment run"""
     parser = argparse.ArgumentParser(description="Experiment arguments")
-    parser.add_argument("--id", help="Specify random seed")
+    parser.add_argument("--id", help="Specify id")
+    parser.add_argument(
+        "--seeds", default=[], help="Specify seeds", type=int, nargs="*"
+    )
 
     args, unknown = parser.parse_known_args()
     args = vars(args)
@@ -26,7 +34,9 @@ def parse_run():
 
 def main():
     args = parse_run()
-    settings_df = pd.read_csv("results/other/settings.csv").set_index('id').to_dict("index")
+    settings_df = (
+        pd.read_csv("results/other/settings.csv").set_index("id").to_dict("index")
+    )
     settings = settings_df[int(args["id"])]
 
     # Load in model
@@ -41,12 +51,27 @@ def main():
         model.set_model()
         root_path = f"results/exp_{args['id']}/lightning_logs"
         long_path = f"{root_path}/{os.listdir(root_path)[0]}/checkpoints"
-        model.model.load_from_checkpoint(
-            f"{long_path}/{os.listdir(long_path)[0]}"
-        )
+        model.model.load_from_checkpoint(f"{long_path}/{os.listdir(long_path)[0]}")
 
-    if settings['problem']=='CVRP':
-        print('working')
+    results = {}
+    for seed in args["seeds"]:
+        results[seed] = {}
+        seed_everything(seed, workers=True)
+        if settings["problem"] == "CVRP":
+            results[seed]["results"], results[seed]["routes"] = utils.test_cvrp(
+                settings["method"],
+                {
+                    "init_method": settings["init_method"],
+                    "improve_method": settings["improve_method"],
+                },
+                args["id"],
+                ast.literal_eval(settings["testing"]),
+                model,
+                save=False,
+            )
+
+    with open(f"results/exp_{args['id']}/results_extra.json", "w") as f:
+        json.dump(results, f, indent=2)
 
 
 if __name__ == "__main__":
