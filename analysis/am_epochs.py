bca5b6e6e5823b8df8245753dd297a6977da6e7e
abigailhayes <abigail.joy.hayes@gmail.com>
2024-07-25 14:31:26 +0200

    Adjust epochs settings

--- analysis/am_epochs.py
+++ analysis/am_epochs.py
@@ -8,7 +8,7 @@ import os
 import json
 
 args = utils.parse_experiment()
-ident=0
+ident = 0
 experiment_dir = f"results/exp_{str(ident)}"
 if not os.path.exists(experiment_dir):
     os.makedirs(experiment_dir)
@@ -16,13 +16,13 @@ test_results = {}
 test_routes = {}
 
 model = rl4co_run.RL4CO(
-            args["problem"],
-            args["method_settings"]["init_method"],
-            args["method_settings"]["customers"],
-            args["seed"],
-            ident,
-            "greedy",
-        )
+    args["problem"],
+    args["method_settings"]["init_method"],
+    args["method_settings"]["customers"],
+    args["seed"],
+    ident,
+    "greedy",
+)
 model.set_model()
 
 test_results[0], test_routes[0] = utils.test_cvrp(
@@ -32,10 +32,10 @@ test_results[0], test_routes[0] = utils.test_cvrp(
     args["testing"],
     model,
     save=False,
-    )
+)
 
 # Checkpointing callback: save models when validation reward improves
-checkpoint_callback = ModelCheckpoint(dirpath='./checkpoints/last.ckpt')
+checkpoint_callback = ModelCheckpoint(dirpath="./checkpoints/last.ckpt")
 
 # Print model summary
 rich_model_summary = RichModelSummary(max_depth=3)
@@ -43,15 +43,23 @@ rich_model_summary = RichModelSummary(max_depth=3)
 # Callbacks list
 callbacks = [checkpoint_callback, rich_model_summary]
 
-for epoch in range(2):
-    ref = epoch+1
+for epoch in [1, 2, 5, 10]:
     trainer_kwargs = {
         "accelerator": "auto",
         "default_root_dir": f"results/am_epochs",
     }
-    model.trainer = RL4COTrainer(max_epochs=ref, **trainer_kwargs, callbacks=callbacks)
-    model.trainer.fit(model.model, ckpt_path='last')
-    test_results[ref], test_routes[ref] = utils.test_cvrp(
+    model = rl4co_run.RL4CO(
+        args["problem"],
+        args["method_settings"]["init_method"],
+        args["method_settings"]["customers"],
+        args["seed"],
+        ident,
+        "greedy",
+    )
+    model.set_model()
+    model.trainer = RL4COTrainer(max_epochs=epoch, **trainer_kwargs, callbacks=callbacks)
+    model.trainer.fit(model.model, ckpt_path="last")
+    test_results[epoch], test_routes[epoch] = utils.test_cvrp(
         args["method"],
         args["method_settings"],
         ident,
@@ -60,7 +68,7 @@ for epoch in range(2):
         save=False,
     )
 
-with open(f"results/am_epochs/results.json", "w") as f:
+with open(f"results/am_epochs/results_{args['method_settings']['init_method']}_{args['method_settings']['customers']}.json", "w") as f:
     json.dump(test_results, f, indent=2)
-with open(f"results/am_epochs/routes.json", "w") as f:
+with open(f"results/am_epochs/routes_{args['method_settings']['init_method']}_{args['method_settings']['customers']}.json", "w") as f:
     json.dump(test_routes, f, indent=2)
