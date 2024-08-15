2c7c235a4edf932c85151811101157db7524bc38
abigailhayes <abigail.joy.hayes@gmail.com>
2024-08-15 15:31:19 +0200

    Adapt vehicle counting

--- analysis/vehicles.py
+++ analysis/vehicles.py
@@ -1,6 +1,8 @@
 import json
 import pandas as pd
 
+from analysis.utils import check_instances
+
 
 def vehicle_count(routes):
     output = 0
@@ -10,55 +12,57 @@ def vehicle_count(routes):
     return output
 
 
-def avg_vehicle_count(folder, nazari=False):
-    if nazari:
-        count_greedy = []
-        count_beam = []
-        for key in folder["greedy"]:
-            count_greedy.append(vehicle_count(folder["greedy"][key]))
-            count_beam.append(vehicle_count(folder["beam"][key]))
-        return (sum(count_greedy) / len(count_greedy)), (
-            sum(count_beam) / len(count_beam)
+def avg_vehicle_count(folder):
+    count = []
+    for key in folder:
+        count.append(vehicle_count(folder[key]))
+    return sum(count) / len(count)
+
+
+def all_vehicle_counts(experiment, validated=True):
+    if validated and experiment != 'c':
+        instance_count = pd.read_csv("results/other/validate_count.csv")
+    else:
+        instance_count = pd.read_csv("results/other/instance_count.csv")
+
+    if experiment == "a":
+        include = instance_count[["A", "B", "E", "F", "M", "P", "CMT"]]
+        include = include.drop(index=0, axis=0)
+    elif experiment == "b":
+        include = instance_count.drop(
+            ["A", "B", "E", "F", "M", "P", "CMT", "id", "notes"], axis=1
         )
-    else:
-        count = []
-        for key in folder:
-            count.append(vehicle_count(folder[key]))
-        return sum(count) / len(count)
+        include = include.drop(index=0, axis=0)
+    elif experiment == "c":
+        include = instance_count.drop(["id", "notes"], axis=1)
 
+    for column_name in list(include):
+        include[column_name] = check_instances(include, column_name)
+    include["id"] = instance_count["id"]
+    include["notes"] = instance_count["notes"]
 
+    for index, row in include.iterrows():
+        print(row["id"])
+        if experiment == "c":
+            json_path = f"results/exp_{row['id']}/routes.json"
+        else:
+            json_path = f"results/exp_{row['id']}/routes_{experiment}.json"
+        try:
+            with open(json_path) as json_data:
+                data = json.load(json_data)
+            if pd.isna(row["notes"]):
+                for key in data:
+                    if row[key] == 1:
+                        include.loc[index, key] = avg_vehicle_count(data[key])
+            elif row["notes"] in ["greedy", "beam"]:
+                for key in data:
+                    if row[key] == 1:
+                        include.loc[index, key] = avg_vehicle_count(
+                            data[key][row["notes"]]
+                        )
 
-def add_vehicle_count(ident, experiment):
-    settings_df = pd.read_csv("results/other/settings.csv")
+        except ValueError:
+            # When none of the tests have been run
+            pass
 
-    if experiment == "c":
-        json_path = f"results/exp_{ident}/routes.json"
-    else:
-        json_path = f"results/exp_{ident}/routes_{experiment}.json"
-
-    if (settings_df[settings_df["id"] == ident]["method"] == "nazari").item():
-        with open(json_path) as json_data:
-            data = json.load(json_data)
-        # When data is split between beam and greedy
-        output_greedy = {"id": ident, "notes": "greedy"}
-        output_beam = {"id": ident, "notes": "beam"}
-
-        for key in data:
-            output_greedy[key], output_beam[key] = avg_vehicle_count(
-                data[key], nazari=True
-            )
-        return pd.concat(
-            [
-                pd.DataFrame.from_dict([output_greedy]),
-                pd.DataFrame.from_dict([output_beam]),
-            ],
-            ignore_index=True,
-        )
-
-    else:
-        with open(json_path) as json_data:
-            data = json.load(json_data)
-        output = {"id": ident}
-        for key in data:
-            output[key] = avg_vehicle_count(data[key])
-        return pd.DataFrame.from_dict([output])
+    include.to_csv(f"results/other/expt_{experiment}_vehicles.csv", index=False)
